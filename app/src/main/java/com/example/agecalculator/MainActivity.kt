0902fa6327e4a475169d28e2a9bd7b36a1b17936
stasvitalich <stasvitalich1994@gmail.com>
2023-02-10 22:56:36 +0600

    Logic to button was added. Icon was changed.

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.agecalculator
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 import com.example.agecalculator.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
@@ -23,17 +22,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clickDatePicker() {
+    private fun clickDatePicker() {
 
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(
-            this,{ view, year, month, dayOfMonth -> },
+        val dpd = DatePickerDialog(
+            this, { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val myDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                binding.textDate.text = myDate
+
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.GERMAN)
+                val newFormat = dateFormat.parse(myDate)
+
+                // How much time has been passed between selected day and 01/01/1970
+                val minBetween =
+                    newFormat.time / 60000 // Divided by 60000 because we need to get a result in minutes
+
+                // How much time has been passed between now and 01/01/1970
+                val currentDate = dateFormat.parse(dateFormat.format(System.currentTimeMillis()))
+                val currentDateInMinutes =
+                    currentDate.time / 60000 // Divided by 60000 because we need to get a result in minutes
+
+                val difference = currentDateInMinutes - minBetween
+                binding.textDate2.text = difference.toString()
+            },
             year,
             month,
             day
-        ).show()
+        )
+
+        /*Through the following line, we limit our calendar.
+        You won't be able to choose a future date, because it
+        contradicts the logic of our application.*/
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000 // Minus current date.
+        dpd.show()
     }
 }

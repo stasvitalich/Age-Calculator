e5a4d28e8b812f7547fee2dcdef0c2a10432f959
stasvitalich <stasvitalich1994@gmail.com>
2023-02-10 18:47:12 +0600

    Date picker was added

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 import com.example.agecalculator.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(s)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.selectButton.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this,{ view, year, month, dayOfMonth -> },
+            year,
+            month,
+            day
+        ).show()
     }
 }
